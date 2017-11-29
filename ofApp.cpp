bc48097f994ab13684f206a6d7b8780cf2e62bd9
KUCCHi <kuchiba_ma_sa1030@outlook.jp>
2017-11-29 23:05:14 +0900

    テンプレートに戻しました

--- ofApp.cpp
+++ ofApp.cpp
@@ -4,28 +4,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	ofSetFrameRate(60);
-	ofBackground(0);
 
-	ofSetBackgroundAuto(false);
-	ofEnableBlendMode(OF_BLENDMODE_ADD);
-}
-
-//--------------------------------------------------------------
-void ofApp::update() {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::draw() {
-	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
-	ofSetColor(0, 0, 0, 3);
-	ofRect(0, 0, ofGetWidth(), ofGetHeight());
-	ofEnableBlendMode(OF_BLENDMODE_ADD);
-	ofSetColor(255, 255, 255, 63);
-	for (int i = 0; i < 100; i++) {
-		walker[i].draw();
-	}
 }
 
 //--------------------------------------------------------------
